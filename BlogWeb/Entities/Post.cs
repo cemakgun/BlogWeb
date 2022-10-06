f09ee4a259ba3a0cb5978a1423876160e46e81a3
Cem Akgün <cemakgun84@gmail.com>
2022-10-06 11:31:28 +0300

    Entities have been created

---
+++ BlogWeb/Entities/Post.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace BlogWeb.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        [Display(Name = "Başlık"), StringLength(100), Required(ErrorMessage = "Bu alan gereklidir!")]
+        public string Title { get; set; }
+        [Display(Name = "Açıklama")]
+        public string? Description { get; set; }
+        [Display(Name = "Resim"), StringLength(100)]
+        public string? Image { get; set; }
+        [Display(Name = "Durum")]
+        public bool IsActive { get; set; }
+        [Display(Name = "Anasayfada Göster")]
+        public bool IsHome { get; set; }
+        [Display(Name = "Eklenme Tarihi"), ScaffoldColumn(false)]
+        public DateTime? CreateDate { get; set; } = DateTime.Now;
+        [Display(Name = "Kategori")]
+        public int? CategoryId { get; set; }
+        [Display(Name = "Kategori")]
+        public virtual Category? Category { get; set; }
+    }
+}
---
+++ BlogWeb/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace BlogWeb.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Display(Name = "Adı"), StringLength(50)]
+        public string? Name { get; set; }
+        [Display(Name = "Soyadı"), StringLength(50)]
+        public string? Surname { get; set; }
+        [StringLength(50)]
+        public string? Email { get; set; }
+        [Display(Name = "Kullanıcı Adı"), StringLength(50), Required(ErrorMessage = "Bu alan gereklidir!")]
+        public string Username { get; set; }
+        [Display(Name = "Şifre"), StringLength(50), Required(ErrorMessage = "Bu alan gereklidir!")]
+        public string Password { get; set; }
+        [Display(Name = "Durum")]
+        public bool IsActive { get; set; }
+        [Display(Name = "Admin")]
+        public bool IsAdmin { get; set; }
+    }
+}
